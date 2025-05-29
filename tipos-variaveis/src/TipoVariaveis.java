472537f398f10a1ffac22b62cbd9f803538b67ff
guilherme <guif.pimental@gmail.com>
2025-05-29 15:50:00 -0300

    add tipo de variavel e operadpres

--- tipos-variaveis/src/TipoVariaveis.java
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -1,5 +1,15 @@
 public class TipoVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+       
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        // queste quando um numero maior e colocado em um tipo que nao suporta
+
+        // para uma variavel se tornar uma constante nececita ser 'final' e estar em caixa alte
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
---
+++ tipos-variaveis/src/operadores.java
@@ -0,0 +1,26 @@
+public class operadores {
+    
+    public static void main(String[] args) {
+        
+String nomeCompleto = "LIGUAGUEM" + "JAVA";
+
+String contatenacao = "?";
+contatenacao = 1 + 1 + 1 + "1";
+
+System.out.println(contatenacao);
+
+contatenacao = 1 + "1" + 1 + 1;
+
+System.out.println(contatenacao);
+
+contatenacao = "1" + 1 + 1 + 1;
+
+System.out.println(contatenacao);
+
+contatenacao = "1" + (1 + 1 + 1);
+
+System.out.println(contatenacao);
+
+    }
+
+}
